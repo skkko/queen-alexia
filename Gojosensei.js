1656d2565537d58c217881cf72d46e5a01a9aaeb
Nep-28 <107740473+Nep-28@users.noreply.github.com>
2022-06-20 23:20:18 +0530

    Update Gojosensei.js

--- Gojosensei.js
+++ Gojosensei.js
@@ -2148,8 +2148,7 @@ break
             break
        	case 'gytmp4b': {
 		if (!text) throw `Example : ${prefix + command}lily`
-		let search = await yts(text)
-		let search = await yts(text)
+		let urls = quoted.text.match(new RegExp(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)/, 'gi'))
 		let anu = search.videos[Math.floor(Math.random() * search.videos.length)]
 		
 		let butttons = [
